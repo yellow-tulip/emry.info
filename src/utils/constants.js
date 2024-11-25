2abef029ee068ddf8bb30f3bc3b26bf7a99de77a
mre <mre@mres-Air-2.lan>
2024-11-24 19:19:59 -0500

    Update URLs to include bucket name 'portfolio'

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const getAssetUrl = (filename) => {
-  return `${import.meta.env.VITE_R2_BUCKET_URL}/${filename}`;
+  return `${import.meta.env.VITE_R2_BUCKET_URL}/portfolio/${filename}`;
 };
