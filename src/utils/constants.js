6f2a080d86ac2c55bbdc70609f807b1e66ab3a3b
mre <mre@mres-Air-2.lan>
2024-11-24 19:28:58 -0500

    Fix URLs to match R2 bucket directory structure

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const getAssetUrl = (filename) => {
   const baseUrl = import.meta.env.VITE_R2_BUCKET_URL;
-  const url = `${baseUrl}/${filename}`;
+  const url = `${baseUrl}/portfolio/${filename}`;
   console.log('Base URL:', baseUrl);
   console.log('Generated URL:', url);
   return url;
